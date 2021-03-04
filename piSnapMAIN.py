d44be66cde273f3d9d71d55d5b7930ff1d1a25ba
Vicky Wilson <victoriaannewilson@yahoo.co.uk>
2021-03-04 14:20:12 +0000

    1st attempt at layout management

--- piSnapMAIN.py
+++ piSnapMAIN.py
@@ -3,8 +3,6 @@ import sys
 from PyQt5 import QtWidgets as qtw 
 from PyQt5 import QtGui as qtg 
 from PyQt5 import QtCore as qtc 
-from PyQt5 import QAction as qa
-
 
 class PiSnap(qtw.QMainWindow): #declare a method to initialize empty window
     def __init__(self):  #first initialize the super class QWidget
@@ -13,25 +11,35 @@ class PiSnap(qtw.QMainWindow): #declare a method to initialize empty window
     def initUI(self):
         self.setGeometry(100, 100, 400, 300)
         self.setWindowTitle('PiSnap!')
-        self.makeMenu() # run makemenu method 
+        self.makeMenu() # run makemenu method
+        self.addMainWidgets()
+        self.cw = qtw.QWidget()
+        self.setCentralWidget(self.cw)
+        self.vlayout = qtw.QVBoxLayout()
+        self.cw.setLayout(self.vlayout)
+        self.st = qtw.QTabWidget()
+
+
+
+        self.show()
     def makeMenu(self): #create menu
         #create actions for file menu
-        vwopen = QAction('&Open', self)
+        vwopen = qtw.QAction('&Open', self)
         vwopen.setShortcut('Ctrl+O')
-        vwopen.triggered.connect(lambda: QFileDialog.getOPenFileName(self))
-        vwsave = QAction('&Save', self)
-        vwsave.setShortcut('Ctrl+S', self)
-        vwsave.triggered.connect(self.saveToFile)
-        vwsaveas = QAction('Save As', self)
+        vwopen.triggered.connect(lambda:qtw.QFileDialog.getOpenFileName(self))
+        vwsave = qtw.QAction('&Save', self)
+        vwsave.setShortcut('Ctrl+S')
+        vwsave.triggered.connect(self.doSave)
+        vwsaveas = qtw.QAction('Save As', self)
         vwsaveas.setShortcut('Ctrl+Shift+S')
-        vwsaveas.triggered.connect()
-        exit_act = QAction('&Exit', self)
+        vwsaveas.triggered.connect(self.doSaveAs)
+        exit_act = qtw.QAction('&Exit', self)
         exit_act.setShortcut('Ctrl+Q')
         exit_act.triggered.connect(self.close)
 
         #create actions for edit menu
-        undo = QAction('&Undo',self)
-        preferences = QAction('Preferences', self)
+        undo = qtw.QAction('&Undo',self)
+        preferences = qtw.QAction('Preferences', self)
 
         #create menubar
         menu_bar = self.menuBar()
@@ -49,13 +57,21 @@ class PiSnap(qtw.QMainWindow): #declare a method to initialize empty window
         edit_menu.addAction(preferences)
         edit_menu.addAction(undo)
         undo.setShortcut('Ctrl+Z')
+    #def makeStatusBar(self): #create status bar
+        #self.qtw.SetStatusBar(QStatusBar(self))
         
-
-
         self.addWidget()
-        self.show()
+        
     def addWidget(parent):
        label = qtw.QLabel('This is a label',parent)
+       label.setGeometry(200,200,200,200)
+    def doSave(self):
+        print('Save code here')
+    def doSaveAs(self):
+        print('Save As code here')
+    def addMainWidgets(self):
+        print('add main widgets')
+    
 
 #end of class
 
