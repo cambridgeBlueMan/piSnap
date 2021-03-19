e414f65d7186da552a535cf5430f833dcc956817
Lea Nicholson <lea@jayls.com>
2021-03-19 20:30:58 +0000

    joystick bullet point constrained by parent widget size. emits signals sendX and sendY when moved. There are no slots defined yet

--- dragbutton.py
+++ dragbutton.py
@@ -1,16 +1,29 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtGui as qtg
+from PyQt5 import QtWidgets as qtw
 
 
-class DragButton(QtWidgets.QPushButton):
-
-    """ ok, so we start by inheriting from QPushButton """
+class DragButton(qtw.QPushButton):
+    xChanged =  qtc.pyqtSignal(int)
+    yChanged = qtc.pyqtSignal(int)
     global newPos
+    def __init__(self, text, win):
+        super().__init__(text, win)
+        #print(self.parent)
+        self.setText("\u26ab")
+        self.setFixedSize(22, 22)
+        self.containerWidth = win.frameGeometry().width() - 22 #800 #win. width
+        self.containerHeight = win.frameGeometry().height() - 22#600 #height
+        #self.xChanged.connect(self.sendX(int))
+        #self.yChanged.connect(self.sendY(int))
+        qtc.QMetaObject.connectSlotsByName(self)
+
     # mousePressEevent
     def mousePressEvent(self, event):
-        print(type(event))
+        #print(type(event))
         self.__mousePressPos = None
         self.__mouseMovePos = None
-        if event.button() == QtCore.Qt.LeftButton:
+        if event.button() == qtc.Qt.LeftButton:
             self.__mousePressPos = event.globalPos()
             self.__mouseMovePos = event.globalPos()
 
@@ -18,36 +31,62 @@ class DragButton(QtWidgets.QPushButton):
 
     def mouseMoveEvent(self, event):
         global newPos
-        if event.buttons() == QtCore.Qt.LeftButton:
+        if event.buttons() == qtc.Qt.LeftButton:
             # adjust offset from clicked point to origin of widget
             currPos = self.mapToGlobal(self.pos())
             globalPos = event.globalPos()
             diff = globalPos - self.__mouseMovePos
 
             newPos = self.mapFromGlobal(currPos + diff)
-            print(newPos)
-            self.move(newPos)
-
+            #print(newPos)
+            # get the x val
+            if newPos.x() > self.containerWidth:
+                x = self.containerWidth
+            elif newPos.x() < 0:
+                x = 0
+            else:
+                x = newPos.x()
+            self.sendX(x)
+            # get the y val
+            if newPos.y() > self.containerHeight:
+                y = self.containerHeight
+            elif newPos.y() < 0:
+                y = 0
+            else:
+                y = newPos.y()
+            self.sendY(y)
+            self.move(x,y)
             self.__mouseMovePos = globalPos
 
+
         super(DragButton, self).mouseMoveEvent(event)
+
+    def sendX(self,x):
+        print(x)
+        self.xChanged.emit(x)
+
+
+    def sendY(self, y):
+        print(y)
+        self.yChanged.emit(y)
 
     def mouseReleaseEvent(self, event):
         if self.__mousePressPos is not None:
             moved = event.globalPos() - self.__mousePressPos 
             if moved.manhattanLength() > 3:
                 event.ignore()
-                print(newPos.x())
+                #print(newPos.x())
                 return
-            print(newPos)
+            #print(newPos)
         super(DragButton, self).mouseReleaseEvent(event)
 
 def clicked():
-    print ("click as normal!")
+    pass
+    # print ("click as normal!")
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    w = QtWidgets.QWidget()
+    app = qtw.QApplication([])
+    w = qtw.QWidget()
     w.resize(800,600)
 
     button = DragButton("Drag", w)
